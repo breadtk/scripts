e52918f641001512ad2ae240f05097ebd85804be
Osman Surkatty <osman@surkatty.org>
2013-12-07 00:34:43 -0800

    Added an open resolver checker script.

---
+++ open_resolver.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+# This script will attempt to discover whether or not an IP address will answer
+# arbitrary DNS queries. This indicates that the server is likely an Open Resolver.
+# For more information about Open Resolvers, visit: http://openresolverproject.org
+
+require 'resolv'
+require 'timeout'
+require 'ipaddr' 
+
+begin 
+  dns_server = IPAddr.new(ARGV[0].chomp).to_s
+rescue IPAddr::InvalidAddressError
+  puts "'#{ARGV[0]}' is not a valid IP address."
+  exit 1
+end
+
+begin
+  Timeout::timeout(3){
+    Resolv::DNS.open({:nameserver=>[dns_server]}) do |r|
+      r.getaddress("surkatty.org")
+      puts "#{dns_server}: open resolve"
+    end
+  }
+rescue Resolv::ResolvError
+  puts "#{dns_server}: maybe non-open"
+rescue Timeout::Error
+  puts "#{dns_server}: timeout"
+end
